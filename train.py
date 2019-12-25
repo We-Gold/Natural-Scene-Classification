f94e267a1de7d063844e24d248a5123f419f7c08
We-Gold <we.goldm@gmail.com>
2019-12-25 06:38:31 -0500

    Switched to vgg16

--- train.py
+++ train.py
@@ -1,6 +1,7 @@
 import keras
 from keras.layers import Dense,GlobalAveragePooling2D
 from keras.applications import MobileNet
+from keras.applications import vgg16
 from keras.models import Model
 from keras.optimizers import Adam
 from keras.callbacks import ModelCheckpoint
@@ -10,12 +11,12 @@ import math
 
 (train_generator, validation_generator) = get_data_generators(os.path.join(os.getcwd(),"data"),(150,150),128,"categorical")
 
-base_model = MobileNet(weights = 'imagenet',include_top = False) 
+base_model = vgg16(weights = 'imagenet',include_top = False) 
 
 x = base_model.output
 x = GlobalAveragePooling2D()(x)
-x = Dense(512,activation = 'relu')(x) 
-x = Dense(256,activation = 'relu')(x)
+x = Dense(256,activation = 'relu')(x) 
+x = Dense(512,activation = 'relu')(x)
 preds = Dense(6,activation = 'softmax')(x)
 
 model = Model(inputs = base_model.input,outputs = preds)
@@ -36,9 +37,9 @@ total_test = count_images(os.path.join(os.getcwd(),"data/test"))
 
 model.fit_generator(train_generator,
                         steps_per_epoch=math.floor(total_train/128),
-                        epochs=5,
+                        epochs=20,
                         validation_data=validation_generator,
                         validation_steps=math.floor(total_test/128),
                         callbacks = [checkpoints])
 
-model.save("model.h5")
+model.save("vgg-model.h5")
